8ef69c65dab85592b05e21619d0b2994367e8885
fitriadidio@gmail.com <fitriadidio@gmail.com>
2023-07-25 01:51:34 +0700

    eslint

--- src/pages/api/axiosInstance.ts
+++ src/pages/api/axiosInstance.ts
@@ -1,23 +1,23 @@
-import axios from 'axios';
-import { useRouter } from 'next/router';
+// import axios from 'axios';
+// import { useRouter } from 'next/router';
 
-const instance = axios.create({
-  baseURL: 'https://dummyjson.com',
-  withCredentials: true,
-});
+// const instance = axios.create({
+//   baseURL: 'https://dummyjson.com',
+//   withCredentials: true,
+// });
 
-// Create a new router instance
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const router = useRouter();
+// // Create a new router instance
+// // eslint-disable-next-line react-hooks/rules-of-hooks
+// const router = useRouter();
 
-instance.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response.status === 401) {
-      // If we get a 401, redirect the user to the login page
-      router.push('/login');
-    }
+// instance.interceptors.response.use(
+//   response => response,
+//   error => {
+//     if (error.response.status === 401) {
+//       // If we get a 401, redirect the user to the login page
+//       router.push('/login');
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
